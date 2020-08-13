655377377326f62ee31d0281e61ba52d962b518a
LX <69516995+lxeh20081109@users.noreply.github.com>
2020-08-14 00:57:33 +0900

    Add files via upload

---
+++ levelC_51.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class levelC_51 {
+	public static void main(String[] args) {
+			        Scanner scanner = new Scanner(System.in);
+	
+			        for (int i = 0; i < 5; i++) System.out.println(scanner.next());
+			        scanner.close();
+			    
+
+	}
+
+}
+
